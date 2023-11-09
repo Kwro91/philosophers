2ae5f4f7ef6617c942c6af08206a07fd102c2b23
besalort <benjamin.salort@hotmail.com>
2023-11-09 18:58:30 +0100

    debug

--- philo/src/thread/action.c
+++ philo/src/thread/action.c
@@ -6,7 +6,7 @@
 /*   By: besalort <besalort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:28:16 by besalort          #+#    #+#             */
-/*   Updated: 2023/11/09 18:06:56 by besalort         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:57:40 by besalort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	print_action(char *action, t_philo *philo)
 {
-	pthread_mutex_lock(philo->print);
+	pthread_mutex_lock(philo->is_dead);
 	if (*philo->dead != 1)
 		printf("%06ld %i %s\n", get_time(philo), philo->indice + 1, action);
-	pthread_mutex_unlock(philo->print);
+	pthread_mutex_unlock(philo->is_dead);
 }
 
 void	philo_sleep(t_philo *philo)
@@ -25,15 +25,15 @@ void	philo_sleep(t_philo *philo)
 	pthread_mutex_lock(philo->is_dead);
 	if (*philo->dead == 0)
 	{
-		print_action("is sleeping", philo);
 		pthread_mutex_unlock(philo->is_dead);
+		print_action("is sleeping", philo);
 		usleep(philo->time.time_sleep.tv_usec);
 	}
 	else
 		pthread_mutex_unlock(philo->is_dead);
 }
 
-void	philo_eat(t_philo *philo)
+void	philo_take_fork(t_philo *philo)
 {
 	pthread_mutex_lock(philo->is_dead);
 	if (*philo->dead == 0)
@@ -43,17 +43,29 @@ void	philo_eat(t_philo *philo)
 		print_action("has taken a fork", philo);
 		pthread_mutex_lock(philo->r_fork);
 		print_action("has taken a fork", philo);
-		pthread_mutex_lock(&philo->eating);
-		gettimeofday(&philo->time.last_meal, NULL);
-		print_action("is eating", philo);
-		usleep(philo->time.time_eat.tv_usec);
-		philo->meal += 1;
-		pthread_mutex_unlock(philo->l_fork);
-		pthread_mutex_unlock(philo->r_fork);
-		pthread_mutex_unlock(&philo->eating);
 	}
 	else
 		pthread_mutex_unlock(philo->is_dead);
+}
+
+void	philo_eat(t_philo *philo)
+{
+	philo_take_fork(philo);
+	pthread_mutex_lock(&philo->eating);
+	pthread_mutex_lock(philo->is_dead);
+	if (*philo->dead == 0)
+	{
+		gettimeofday(&philo->time.last_meal, NULL);
+		pthread_mutex_unlock(philo->is_dead);
+		print_action("is eating", philo);
+		pthread_mutex_unlock(&philo->eating);
+		usleep(philo->time.time_eat.tv_usec);
+		philo->meal += 1;
+	}
+	else
+		pthread_mutex_unlock(philo->is_dead);
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
 }
 
 void	philo_think(t_philo *philo)
