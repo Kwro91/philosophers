f0db05bbb2f94a8a0a799736cf6735f93798d3cd
besalort <benjamin.salort@hotmail.com>
2023-11-09 19:24:17 +0100

    debug

--- philo/src/thread/action.c
+++ philo/src/thread/action.c
@@ -6,7 +6,7 @@
 /*   By: besalort <besalort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:28:16 by besalort          #+#    #+#             */
-/*   Updated: 2023/11/09 18:57:40 by besalort         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:18:10 by besalort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,30 +35,36 @@ void	philo_sleep(t_philo *philo)
 
 void	philo_take_fork(t_philo *philo)
 {
-	pthread_mutex_lock(philo->is_dead);
+	// pthread_mutex_lock(philo->is_dead);
 	if (*philo->dead == 0)
 	{
-		pthread_mutex_unlock(philo->is_dead);
-		pthread_mutex_lock(philo->l_fork);
+		// pthread_mutex_unlock(philo->is_dead);
+		if (philo->indice % 2)
+			pthread_mutex_lock(philo->l_fork);
+		else
+			pthread_mutex_lock(philo->r_fork);
 		print_action("has taken a fork", philo);
-		pthread_mutex_lock(philo->r_fork);
+		if (philo->indice % 2)
+			pthread_mutex_lock(philo->r_fork);
+		else
+			pthread_mutex_lock(philo->l_fork);
 		print_action("has taken a fork", philo);
 	}
-	else
-		pthread_mutex_unlock(philo->is_dead);
+	// else
+		// pthread_mutex_unlock(philo->is_dead);
 }
 
 void	philo_eat(t_philo *philo)
 {
 	philo_take_fork(philo);
-	pthread_mutex_lock(&philo->eating);
 	pthread_mutex_lock(philo->is_dead);
 	if (*philo->dead == 0)
 	{
-		gettimeofday(&philo->time.last_meal, NULL);
 		pthread_mutex_unlock(philo->is_dead);
-		print_action("is eating", philo);
+		pthread_mutex_lock(&philo->eating);
+		gettimeofday(&philo->time.last_meal, NULL);
 		pthread_mutex_unlock(&philo->eating);
+		print_action("is eating", philo);
 		usleep(philo->time.time_eat.tv_usec);
 		philo->meal += 1;
 	}
